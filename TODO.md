417d25d6f6b6a431e1be1769970cc7ffa78eb626
Magnesium- <yyong007@outlook.com>
2023-09-24 10:56:03 +0800

    feat: eslint use node env

--- TODO.md
+++ TODO.md
@@ -1,5 +1,9 @@
 # TODO
 
+## watch
+
+- [x] nodemon start
+
 ## staic
 
 ## body
--- src/config.js
+++ src/config.js
@@ -2,7 +2,5 @@ import "dotenv/config";
 
 export class AppConfig {
   port = process.env.APP_PORT || 3000;
-  constructor(options) {
-    console.log(process.env.APP_PORT);
-  }
+  constructor(options) {}
 }
